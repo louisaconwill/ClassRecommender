a13e4e2f106649075803b8cd585f0a70de3b1d34
Sahakait Benyasut <sahakait@Sahakaits-MacBook-Pro.local>
2016-02-07 00:29:03 -0500

    Added HomeViewController

--- iOS/ClassRecommender/HomeViewController.swift
+++ iOS/ClassRecommender/HomeViewController.swift
@@ -7,11 +7,22 @@
 //
 
 import UIKit
+import LFReversibleTinderView
 
-class HomeViewController: UIViewController {
+class HomeViewController: UIViewController, MDCSwipeToChooseDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let options = MDCSwipeToChooseViewOptions()
+                 options.delegate = self
+                 options.likedText = "Keep"
+                 options.likedColor = UIColor.blackColor()
+                 options.nopeText = "Delete"
+        
+                 let mdcview = MDCSwipeToChooseView(frame:self.view.bounds, options: options)
+        
+                 self.view.addSubview(mdcview) //adds swiping object to view
         
         Constants.userRef.observeSingleEventOfType(.Value, withBlock: { snapshot in
             if let firstTime = snapshot.value.objectForKey("firstTime") as? String {
